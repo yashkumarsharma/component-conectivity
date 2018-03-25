9f6ec8e98f55b5d3171306b782026a77bfd65676
Rahul <mail4rahul.tiwari@gmail.com>
2018-03-26 01:31:50 +0530

    Application - Inprogress.

--- src/application/application.js
+++ src/application/application.js
@@ -9,64 +9,49 @@ class Application extends Component {
 				{'Name': 'Rahul', 'Recent': 2, 'Alltime': 1992, 'Activity': '20:58:50 February 20, 2018', 'url': 'https://www.google.co.in'  },
 				{'Name': 'Yash', 'Recent': 14, 'Alltime': 1991, 'Activity': '19:28:50 February 20, 2018', 'url': 'https://www.google.co.in'  },
 				{'Name': 'Shashank', 'Recent': 24, 'Alltime': 1990, 'Activity': '20:19:51 February 20, 2018', 'url': 'https://www.google.co.in'  },
-				{'Name': 'Divya', 'Recent': 12, 'Alltime': 1988, 'Activity': '21:50:40 February 20, 2018', 'url': 'https://www.google.co.in'  }			
+				{'Name': 'Divya', 'Recent': 2, 'Alltime': 1988, 'Activity': '21:50:40 February 20, 2018', 'url': 'https://www.google.co.in'  }			
 			]
 		this.state = {
 			count: 0,
-			activeRecent: false,
-			activeAlltime: false,
-			activeSortRecent: false,
-			activeSortAlltime: false
+			activeItem: 'recent'
 		}
 	}
 
-	loadDefault = () => {
-		const arr = []
-		this.array.map(function(value, index){
-			arr.push(
+	loadDefault = (arr) => {
+		const newArr = []
+		arr.map(function(value, index){
+			newArr.push(
 				<tr key={index}>
 					<td key="{value.name}"><a href={value.url}>{value.Name}</a></td><td key="{value.Recent}">{value.Recent}</td>
 					<td key="{value.Alltime}">{value.Alltime}</td><td key="{value.Activity}">{value.Activity}</td>
 				</tr>
 			)
 		})
-		return <tbody>{arr}</tbody>
+		return <tbody>{newArr}</tbody>
 	} 
 
-	showRecent = () => {
-	 	const arr = []
-		this.array.sort((a, b) => a.Recent < b.Recent)
-		.map(function(value, index){
-			arr.push(value)
-		})
-		this.setState({array: arr, activeRecent: true, activeAlltime: false, activeSortRecent: false, activeSortAlltime: false})
-	}
-
-	showAllTime = () => {
-	 	const arr = []
-			this.array.sort((a, b) => a.Alltime > b.Alltime)
-			.map(function(value, index){
-				arr.push(value)
+  loadData = (arr , key , sortType) => {
+  	const newArr = []
+  	if(sortType==='accending') {
+	  	arr.sort((a, b) => key.a < key.b)
+	  	.map(function(value, index){
+				newArr.push(value)
 			})
-		this.setState({array: arr, activeRecent: false, activeAlltime: true, activeSortRecent: false, activeSortAlltime: false})
-  }
+			return true
+			this.setState({array: newArr})
 
-	sortRecent = () => {
-	 	const arr = []
-		this.array.sort((a, b) => a.Recent > b.Recent)
-		.map(function(value, index){
-			arr.push(value)
-		})
-		this.setState({array: arr, activeRecent: false, activeAlltime: false, activeSortRecent: true, activeSortAlltime: false})
-	}
+		}
 
-	sortAlltime = () => {
-	 	const arr = []
-			this.array.sort((a, b) => a.Alltime < b.Alltime)
-			.map(function(value, index){
-				arr.push(value)
+		if(sortType==='decending') {
+			console.log(123)
+			arr.sort((a, b) => key.a > key.b)
+	  	.map(function(value, index){
+				newArr.push(value)
 			})
-		this.setState({array: arr, activeRecent: false, activeAlltime: false, activeSortRecent: false, activeSortAlltime: true})
+			return true
+			this.setState({array: newArr})
+
+		}
   }
 
 	render() {
@@ -74,10 +59,26 @@ class Application extends Component {
 			<div>
 			<h1>Application</h1>
 			<div className="button-wrapper">
-				<button className={"button " + (this.state.activeRecent ? 'active' : '')} onClick={this.showRecent} >Show Recent</button>
-				<button className={"button " + (this.state.activeAlltime ? 'active' : '')}  onClick={this.showAllTime}>Show Alltime</button>
-				<button className={"button " + (this.state.activeSortRecent ? 'active' : '')}  onClick={this.sortRecent}>Sortby Recent</button>
-				<button className={"button " + (this.state.activeSortAlltime ? 'active' : '')}  onClick={this.sortAlltime}>Sortby Alltime</button>
+				<button 
+				className={"button " + (this.state.activeItem === 'recent' ? 'active' : '')} 
+				onClick={this.loadData(this.array, this.array.recent, 'decending')} >
+				Show Recent
+				</button>
+				<button 
+				className={"button " + (this.state.activeItem === 'alltime' ? 'active' : '')}  
+				onClick={this.loadData(this.array, this.array.alltime, 'decending')} >
+				Show Alltime
+				</button>
+				<button 
+				className={"button " + (this.state.activeItem === 'sortRecent' ? 'active' : '')}  
+				onClick={this.sortRecent}>
+				Sortby Recent
+				</button>
+				<button 
+				className={"button " + (this.state.activeItem === 'sortAlltime' ? 'active' : '')}  
+				onClick={this.sortAlltime}>
+				Sortby Alltime
+				</button>
 			</div>
 			<div className="table-wrapper">
 				<table>
@@ -97,7 +98,7 @@ class Application extends Component {
 							</th>
 						</tr>
 					</thead>
-						{this.loadDefault()}
+						{this.loadDefault(this.array)}
 				</table>
 			</div>
 			</div>
